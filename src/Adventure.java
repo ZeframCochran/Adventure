7a2b9f0c7eec2610f8d0076f218a5539d64d291a
Jerod <Anonygram@gmail.com>
2015-03-27 06:30:40 +0000

    Initial Commit

---
+++ src/Adventure.java
@@ -0,0 +1,37 @@
+/**
+Template for the game programming assignment
+Jerod Ewert
+3/26/2015
+*/
+public class Adventure{
+ //Items that can be LOOKed at
+ 
+ //Variables to describe the results of actions
+ 
+ //Variables to track the state of our hero/heroine
+ 
+ //methods
+ public static void main(String[] args){}
+ 
+ //Take user input, trigger the doCommand method
+ public static void getCommands(){}
+ 
+ //Given a command from the user, call the correct function with the needed parameters
+ public static void doCommand(String command){}
+ 
+ //Given an X and Y position return which room the character is in.
+ public static void getRoom(int xPos, int yPos){}
+ 
+ //Move our character to an x and y position
+ public static String move(int x, int y){}
+ 
+ //Returns the description of the item you are LOOKing at
+ public static void look(String item){}
+ 
+ //Returns the description of the room
+ public static void look(){}
+
+//
+ public static void get(String item){}
+}
+}
